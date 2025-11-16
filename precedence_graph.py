7123bb288ce2de0de8b45088784624b816784d73
Katie <ktbrady7@gmail.com>
2025-11-16 18:01:02 -0500

    11 points, need to fix cycles part

---
+++ precedence_graph.py
@@ -0,0 +1,183 @@
+"""
+Author: Rami Pellumbi
+
+This is an implementation of the precedence graph algorithm for conflict
+serializability.
+"""
+from collections import namedtuple
+from typing import Dict, List, Set, Union
+
+# An operation is a tuple of three elements:
+# - transaction: the transaction id (e.g., T1)
+# - type: the operation type (read or write)
+# - item: the item being read or written (e.g., A)
+Operation = namedtuple("Operation", ["transaction", "type", "item"])
+
+"""
+A schedule is a list of steps, where each step is a list of operations.
+
+- Suppose we have m transactions and k steps. We may view the schedule
+  as a matrix of size k x m, where each cell contains the operations of a
+  transaction in a step (may be None). That is, the (i, j)-th cell contains
+  the operation of transaction Tj in step i, 1 <= i <= k, 1 <= j <= m.
+- Two operations in the same step, i.e., op_1 and op_2 are both in the i-th
+  row, may run in any order.
+- Two operations in different steps, i.e., op_1 is in the i-th row and op_2
+  is in the j-th row, and i < j, must run in the order op_1 followed by op_2.
+"""
+Schedule = List[List[Union[Operation, None]]]
+
+
+class Node:
+    """
+    Represents a node in the graph, which is a transaction in this context.
+    """
+
+    def __init__(self, transaction_id):
+        self.id = transaction_id
+        # edges is a set of destination nodes from this node
+        self.edges: Set["Node"] = set()
+
+    def add_edge(self, destination_node: "Node"):
+        """Add an edge from this node to another."""
+        self.edges.add(destination_node)
+
+    def __repr__(self):
+        """String representation for debugging."""
+        return f"Node({self.id})"
+
+
+class PrecedenceGraph:
+    """
+    Class to represent a precedence graph given a schedule
+    """
+
+    def __init__(self, schedule: Schedule) -> None:
+        # maps a transaction id (e.g., T1) to a Node object
+        self._nodes: Dict[str, Node] = {}
+        # flag to check if the graph has been initialized
+        self.is_initialized = False
+        # initialize self._nodes and set self.is_initialized to True
+        self._initialize_nodes(schedule)
+        # build the precedence graph from the schedule
+        self._build_graph(schedule)
+
+    def _add_node_if_not_exists(self, transaction_id):
+        """Add a node to the graph if it is not already present."""
+        if transaction_id not in self._nodes:
+            self._nodes[transaction_id] = Node(transaction_id)
+
+    def _initialize_nodes(self, schedule: Schedule):
+        """
+        Initialize all transactions in the graph
+        """
+        for step in schedule:
+            for operation in step:
+                if operation is not None:
+                    tx = operation.transaction
+                    self._add_node_if_not_exists(tx)
+        self.is_initialized = True
+
+    def _has_conflict(self, operation1: Operation, operation2: Operation) -> bool:
+        """
+        Determine if two operations conflict.
+
+        A conflict occurs if two different transactions are operating
+        on the same item and at least one of them is a write operation.
+
+        Returns:
+            True if the operations conflict, False otherwise.
+        """
+        # An operation is a tuple of three elements:
+        # - transaction: the transaction id (e.g., T1)
+        # - type: the operation type (read or write)
+        # - item: the item being read or written (e.g., A))
+
+        # TODO: Implement this function to check if two operations conflict
+        # Hint: compute three boolean values and return their conjunction
+        operation_type_1 = operation1[1]
+        item_1 = operation1[2]
+
+        operation_type_2 = operation2[1]
+        item_2 = operation2[2]
+
+        return item_1 == item_2 and (operation_type_2 == "write" or operation_type_1 == "write")
+
+
+    def _build_graph(self, schedule: Schedule):
+        """
+        Build the precedence graph from the schedule
+
+        TODO: Implement this function to build the precedence graph
+        by following the steps below.
+        """
+        if not self.is_initialized:
+            raise ValueError("Nodes have not been initialized")
+
+        # A schedule is a list of steps, where each step is a list of operations.
+
+        # - Suppose we have m transactions and k steps. We may view the schedule
+        #   as a matrix of size k x m, where each cell contains the operations of a
+        #   transaction in a step (may be None). That is, the (i, j)-th cell contains
+        #   the operation of transaction Tj in step i, 1 <= i <= k, 1 <= j <= m.
+        # - Two operations in the same step, i.e., op_1 and op_2 are both in the i-th
+        #   row, may run in any order.
+        # - Two operations in different steps, i.e., op_1 is in the i-th row and op_2
+        #   is in the j-th row, and i < j, must run in the order op_1 followed by op_2.
+        # """
+        # Schedule = List[List[Union[Operation, None]]]        
+
+        # TODO: Step 1: flatten the schedule into a list of operations,
+        # maintaining the order of the operations
+
+        # basically turn the matrix into a flat list
+        # iterate through the matrix by row, and they can go in any order? 
+        operations = []
+        for i in range(len(schedule)):
+            for j in range(len(schedule[0])): 
+                operations.append(schedule[i][j])
+
+        # TODO: Step 2: add edges between nodes that have conflicts
+        # An edge from node T1 to node T2 means that T1 must come before T2
+        # Hint 1: use the _has_conflict method to check two operations conflict
+        # Hint 2: add an edge from the source node to the destination node
+        # ~8 lines of code
+
+        for i in range(0, len(schedule)-1):
+            for j in range(i+1, len(schedule)):
+                # if(self._has_conflict(operations[i], operations[j]))
+                if(self._has_conflict(operation1=operations[i], operation2=operations[j])):
+                    node1 = Node(operations[i])
+                    node2 = Node(operations[j])
+                    # FIXME: make sure you find it in the graph? reuse node or can be new node?
+                    # is there already source node? 
+                    # what do i return ?
+                    node1.add_edge(node2)
+
+    def __repr__(self) -> str:
+        rep = ""
+        for src, node in self._nodes.items():
+            destinations = node.edges
+            if destinations:
+                # Format: Source -> [Destination1, Destination2, ...]
+                formatted_destinations = ", ".join(dest.id for dest in destinations)
+                rep += f"Source {src} -> [{formatted_destinations}]\n"
+
+            else:
+                # Explicitly state when there are no outgoing edges
+                rep += f"Source {src} has no outgoing edges\n"
+
+        return rep
+
+    @property
+    def nodes(self) -> Dict[str, Node]:
+        """Return the nodes in the graph."""
+        return self._nodes
+
+if __name__ == "__main__":
+    operation1 = Operation(transaction="T1", type="read", item="A")
+    operation2 = Operation(transaction="T2", type="write", item="A")
+    operation3 = Operation(transaction="T3", type="read", item="B")
+    schedule = [[operation1, operation2],[operation2, operation1],[operation3, None]]
+    graph = PrecedenceGraph(schedule)
+    
---
+++ precedence_utils.py
@@ -0,0 +1,142 @@
+"""
+Functions to check if a schedule is conflict serializable.
+"""
+
+from typing import List, Set
+from precedence_graph import PrecedenceGraph 
+
+
+def has_cycles(graph: PrecedenceGraph) -> bool:
+    """
+    Checks if the given graph has cycles.
+
+    Returns:
+        True if the graph has cycles, False otherwise.
+    """
+    # visited is a set of visited nodes
+    visited = set()
+    # rec_stack is a set of nodes in the current recursion stack
+    rec_stack = set()
+
+    def _has_cycles_util(transaction_id: str) -> bool:
+        """
+        Utility function to check if the graph has cycles.
+
+        Returns:
+            True if the graph has cycles, False otherwise.
+
+        TODO: Implement this helper function to check if the graph has cycles
+        by completing the following steps.
+        """
+        # TODO 1: check if the node is in the recursion stack or
+        # has already been visited and return accordingly (4 lines)
+        print("katie starts ")
+        # if(transaction_id in rec_stack or transaction_id in visited):
+        #     print("cycle detected")
+        # if(transaction_id in rec_stack or transaction_id in visited):
+            # return True
+
+        # TODO 2: Mark the node as visited and add it to the recursion stack
+        # Mark the node as visited and add it to the recursion stack (2 lines)
+        # visited.add(transaction_id)
+        # visited.add(transaction_id)
+        # rec_stack.add(transaction_id)
+
+        # TODO 3: Recursively check all neighbours for cycles (~3 lines)
+        # print(graph.nodes[transaction_id])
+        # print("edges")
+        # print(graph.nodes[transaction_id].edges)
+        # print(graph.nodes[transaction_id].edges)
+        # for node in graph.nodes[transaction_id].edges:
+        #     print("hi from neighbor:")
+        #     print(node)
+        #     return _has_cycles_util(node[transaction_id])
+        # FIXME!!!!
+
+        # TODO 4: Remove the node from the recursion stack (1 line)
+        # rec_stack.remove(transaction_id)
+
+        # TODO 5: Return no cycle found
+        return False
+
+    # Check for cycles in all nodes
+    # for tx_id in graph.nodes:
+    #     if _has_cycles_util(tx_id):
+    #         return True
+
+    return False
+
+
+def is_conflict_serializable(precedence_graph: PrecedenceGraph) -> bool:
+    """
+    Check if a schedule is conflict serializable.
+
+
+    Returns:
+        True if the schedule is conflict serializable, False otherwise.
+    """
+    # TODO: 1 line of code
+    # conflict serializable means no cycles?
+    # return False  # Change this line
+    return not has_cycles(precedence_graph)
+
+
+def find_all_topological_sorts(pg: PrecedenceGraph) -> List[List[str]]:
+    """
+    Find all topological sorts of the precedence graph of the schedule
+    """
+    # Return an empty list if the schedule is not conflict serializable
+    if not is_conflict_serializable(pg):
+        return []
+
+    # nodes in the graph (Dictionary of transaction id to Node object)
+    nodes = pg.nodes
+
+    # Initialize in-degrees for each node
+    in_degrees = {node: 0 for node in nodes}
+    for node in nodes.values():
+        for edge in node.edges:
+            in_degrees[edge.id] += 1
+
+    # visited is used to keep track of visited nodes
+    visited: Set[str] = set()
+    # stack is used to keep track of the current topological sort
+    stack: List[str] = []
+    # all_orders is a list of all topological sorts
+    all_orders: List[List[str]] = []
+
+    def _all_topological_sorts_util(visited: Set[str],
+                                    stack: List[str],
+                                    all_orders: List[List[str]]) -> None:
+        """
+        Utility function to find all topological sorts of the graph.
+
+        This function is a recursive backtracking function that explores all
+        possible topological sorts of the graph.
+
+        TODO: Implement this function to find all topological sorts of the
+        graph by completing the following steps.
+        """
+        # TODO 1: Check if all nodes are visited; if so, record the current
+        # order by adding a COPY of the stack to all_orders and return
+
+        # Iterate over all nodes
+        for tx, node in nodes.items():
+            # TODO 2: Proceed only if node is unvisited and has no
+            # incoming edges from unvisited nodes
+            if True:  # Replace True with the correct condition
+                # TODO 3: Visit the node and add it to the topological sort
+                # stack (2 lines)
+
+                # TODO 4: Decrement in-degrees of successors
+                # (2 lines)
+
+                # TODO 5: Recurse (1 line)
+
+                # TODO 6: Backtrack: un-visit the node and restore in-degrees
+                # of successors (4 lines)
+                pass
+
+    _all_topological_sorts_util(visited, stack, all_orders)
+
+    return all_orders
